e0abeba9fdd3990480fc47addf73760b9469dfb5
Samuel Kopp <62482066+unvented@users.noreply.github.com>
2023-03-11 03:12:10 +0100

    fix: remove unused `simple-import-sort` plugin

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint',
-    'unused-imports',
-    'simple-import-sort'
+    'unused-imports'
   ],
   extends: [
     'eslint:recommended',
